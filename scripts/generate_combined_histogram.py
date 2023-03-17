76328dce9774033811fe6036fc07e1b342fdd60b
Duco Gaillard <ducogaillard@gmail.com>
2023-03-17 16:51:13 +0100

    changed combined plot to probability distribution

--- scripts/generate_combined_histogram.py
+++ scripts/generate_combined_histogram.py
@@ -3,10 +3,33 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 def calculate_mean_distribution(metrics_files):
-    combined_data = pd.concat([pd.read_csv(file, sep='\t', names=["insert_size", "count"]) for file in metrics_files])
-    mean_data = combined_data.groupby("insert_size").mean().reset_index()
-    std_data = combined_data.groupby("insert_size").std().reset_index()
+    # Read each file and store in a list of dataframes
+    dataframes = [pd.read_csv(file, sep='\t', names=["insert_size", f"count_{i}"]) for i, file in enumerate(metrics_files)]
+
+    # Merge the dataframes on the insert_size column
+    combined_data = dataframes[0]
+    for df in dataframes[1:]:
+        combined_data = combined_data.merge(df, on="insert_size", how="outer")
+
+    # Fill NaN values with 0
+    combined_data.fillna(0, inplace=True)
+
+    # Divide each column by the column sum
+    for column in combined_data.columns[1:]:
+        combined_data[column] /= combined_data[column].sum()
+
+    # Calculate mean and standard deviation per insert size
+    mean_data = combined_data.iloc[:, 1:].mean(axis=1).to_frame(name="mean")
+
+    std_data = combined_data.iloc[:, 1:].std(axis=1).to_frame(name="std")
+    
+    # Add the insert_size column to the mean and std dataframes
+    mean_data.insert(0, "insert_size", combined_data["insert_size"])
+    std_data.insert(0, "insert_size", combined_data["insert_size"])
+
     return mean_data, std_data
+
+
 
 def plot_combined_histogram(benign_metrics_files, malignant_metrics_files, output_file, plotting_data_file):
     benign_mean, benign_std = calculate_mean_distribution(benign_metrics_files)
@@ -14,11 +37,14 @@ def plot_combined_histogram(benign_metrics_files, malignant_metrics_files, outpu
 
     plt.figure(figsize=(10, 6))
 
-    plt.plot(benign_mean["insert_size"], benign_mean["count"], color='blue', label='Benign')
-    plt.fill_between(benign_mean["insert_size"], benign_mean["count"] - benign_std["count"], benign_mean["count"] + benign_std["count"], color='blue', alpha=0.1)
+    plt.plot(benign_mean["insert_size"], benign_mean["mean"], color='blue', label='Benign')
+    plt.fill_between(benign_mean["insert_size"], benign_mean["mean"] - benign_std["std"], benign_mean["mean"] + benign_std["std"], color='blue', alpha=0.1)
 
-    plt.plot(malignant_mean["insert_size"], malignant_mean["count"], color='red', label='Malignant')
-    plt.fill_between(malignant_mean["insert_size"], malignant_mean["count"] - malignant_std["count"], malignant_mean["count"] + malignant_std["count"], color='red', alpha=0.1)
+    plt.plot(malignant_mean["insert_size"], malignant_mean["mean"], color='red', label='Malignant')
+    plt.fill_between(malignant_mean["insert_size"], malignant_mean["mean"] - malignant_std["std"], malignant_mean["mean"] + malignant_std["std"], color='red', alpha=0.1)
+
+    # add limits
+    # plt.xlim([0,400])
 
     plt.xlabel("Insert Size")
     plt.ylabel("Mean Count")
